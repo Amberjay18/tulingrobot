a9e0b1cfc32e7469591ca02f1995a865422facde
Amberjiang <39793282+Amberjay18@users.noreply.github.com>
2019-03-26 21:57:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,33 @@
 import requests
 import flask
 from flask import request
-from flask import Flask
+from flask import Flask, make_response
 import time
 import json
 import hashlib
 import re
 import xml.etree.ElementTree as ET
 # 引入所需要的库
-
 app = Flask(__name__)
 
 #@app.route("/")
 #return "Hello World!"
 # 默认的
 
-@app.route('/wecrobot', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 
 def wechat_tuling():
     if request.method == 'GET':
         #判断如果请求方式是get
         token = 'tulingrobot'
         # 这里输入你在微信公众号里面填的token
-        my_signature = request.args.get('signature')
+        my_signature = request.args.get('signature', '')
         # 获取携带 signature微信加密签名的参数
-        my_timestamp = request.args.get('timestamp')
+        my_timestamp = request.args.get('timestamp', '')
         # 获取携带 随机数timestamp的参数
-        my_nonce = request.args.get('nonce')
+        my_nonce = request.args.get('nonce', '')
         # 获取携带 时间戳nonce的参数
-        my_echostr = request.args.get('echostr')
+        my_echostr = request.args.get('echostr', '')
         # 获取携带 随机字符串 echostr的参数
         data = [token, my_timestamp, my_nonce]
         # 进行字典排序
@@ -39,20 +38,31 @@ def wechat_tuling():
         # 将三个参数字符串拼接成一个字符串进行sha1加密
         if my_signature == mysignature:
             # 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
-            return my_echostr
+            res = make_response(my_echostr)
+            res.headers['content-type'] = 'text'
+            # 这段代码是为了避免微信token验证失败
+            return res
         else:
-            # 解析xml
-            xml = ET.fromstring(request.data)
-            toUser = xml.find('ToUserName').text
-            fromUser = xml.find('FromUserName').text
-            msgType = xml.find("MsgType").text
-            createTime = xml.find("CreateTime")
-            # 判断类型并回复
-            if msgType == "text":
-                content = xml.find('Content').text
-                return reply_text(fromUser, toUser, reply(fromUser, content))
-            else:
-                return reply_text(fromUser, toUser, "我暂时只会文字，还在学习其他技能，请稍等！")
+            return ''
+
+@app.route('/', methods=['POST'])
+
+def autoreplay():
+
+    xml = ET.fromstring(request.data)
+    toUser = xml.find('ToUserName').text
+    fromUser = xml.find('FromUserName').text
+    msgType = xml.find("MsgType").text
+    createTime = xml.find("CreateTime")
+    if msgType == 'text':
+        content = xml.find('Content').text
+        content = reply(content)
+        resp = make_response(reply_text(fromUser,
+         toUser, reply(fromUser, content)))
+        resp.headers['content-type'] = 'text'
+        return resp
+    else:
+        return reply_text(fromUser, toUser, '暂不支持图片视频语音类')
 
 def reply_text(to_user, from_user, content):
     """
@@ -93,4 +103,4 @@ def reply(info):
         return robot_res["results"][0]['values']['text']
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050)
+    app.run(host='0.0.0.0', port=80)
